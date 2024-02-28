838ae5803950cead5e22f5e83c29ea011ed5dd6b
Kyle Harper <kylesharper@gmail.com>
2024-02-27 21:28:06 -0600

    Wooo

--- DRV8825.py
+++ DRV8825.py
@@ -32,14 +32,6 @@ class DRV8825():
         self.digital_write(self.enable_pin, 0)
     
     def SetMicroStep(self, mode, stepformat):
-        """
-        (1) mode
-            'hardward' :    Use the switch on the module to control the microstep
-            'software' :    Use software to control microstep pin levels
-                Need to put the All switch to 0
-        (2) stepformat
-            ('fullstep', 'halfstep', '1/4step', '1/8step', '1/16step', '1/32step')
-        """
         microstep = {'fullstep': (0, 0, 0),
                      'halfstep': (1, 0, 0),
                      '1/4step': (0, 1, 0),
@@ -47,29 +39,23 @@ class DRV8825():
                      '1/16step': (0, 0, 1),
                      '1/32step': (1, 0, 1)}
 
-        print("Control mode:",mode)
         if (mode == ControlMode[1]):
-            print("set pins")
             self.digital_write(self.mode_pins, microstep[stepformat])
         
     def TurnStep(self, Dir, steps, stepdelay=0.005):
         if (Dir == MotorDir[0]):
-            print("forward")
             self.digital_write(self.enable_pin, 1)
             self.digital_write(self.dir_pin, 0)
         elif (Dir == MotorDir[1]):
-            print("backward")
             self.digital_write(self.enable_pin, 1)
             self.digital_write(self.dir_pin, 1)
         else:
-            print("the dir must be : 'forward' or 'backward'")
             self.digital_write(self.enable_pin, 0)
             return
 
         if (steps == 0):
             return
             
-        print( "turn step:",steps)
         for i in range(steps):
             self.digital_write(self.step_pin, True)
             time.sleep(stepdelay)
--- Enderbot.py
+++ Enderbot.py
@@ -1,25 +1,101 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Load the gamepad and time libraries
 import Gamepad
 import time
+import RPi.GPIO as GPIO
+import time
+import math
+import threading
+import logging
+from DRV8825 import DRV8825
 
-# Gamepad settings
-gamepadType = Gamepad.Xbox360
+def threadedMovement(side):
+    logging.info("Thread for side %s: starting", side)
 
-# Wait for a connection
-if not Gamepad.available():
-    print('Please connect your gamepad...')
-    while not Gamepad.available():
-        time.sleep(1.0)
-gamepad = gamepadType()
-print('Gamepad connected')
+    try:
+        if side == 0:
+            Motor = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
+        else:
+            Motor = DRV8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
+        Motor.SetMicroStep('hardward' ,'1/32step')
+        logging.info("Motor side %s initialized", side)
 
-# Handle joystick updates one at a time
-while gamepad.isConnected():
-    # Wait for the next event
-    eventType, control, value = gamepad.getNextEvent()
+        while True:
+            if side == 0:
+                speed = gamepad.axis('LEFT-Y')
 
-    if eventType == 'AXIS' and control == 'LEFT-Y':
-        print(f'Left: {value}')
+                if speed < 0:
+                    direction='backward'
+                    speed = math.floor(speed * -10)
+                elif speed > 0:
+                    direction ='forward'
+                    speed = math.floor(speed * 10)
+            else:
+                speed = gamepad.axis('RIGHT-Y')
+
+                if speed < 0:
+                    direction='forward'
+                    speed = math.floor(speed * -10)
+                elif speed > 0:
+                    direction ='backward'
+                    speed = math.floor(speed * 10)
+
+            if speed < deadZone:
+                Motor.Stop()
+            else:
+                #logging.info("Rotating %s in direction %s with speed %s", side, direction, stepperDelayMapping[speed-1])
+                Motor.TurnStep(Dir=direction, steps=1, stepdelay = 1 / stepperDelayMapping[speed-1])
+
+            time.sleep(pollInterval)
+
+    except:
+        logging.info("Motor side %s failed to initialize", side)
+        Motor.Stop()
+        exit()
+
+    finally:
+        Motor.stop()
+        logging.info("Thread for side %s: finishing", side)
+
+       
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    # Gamepad settings
+    gamepadType = Gamepad.Xbox360
+    pollInterval = 0.00001
+    deadZone = 0.1
+    stepperDelayMapping = [1000,1000,1000,10000,10000,100000,100000,1000000,1000000,10000000]
+    deadZone = 2
+    leftSpeed = 0.0
+    leftSpeed = 0.0
+
+    # Wait for a connection
+    if not Gamepad.available():
+        logging.info("No gamepad connected")
+        while not Gamepad.available():
+            time.sleep(1.0)
+    gamepad = gamepadType()
+    logging.info("Gamepad connected")
+
+    # Start the background updating
+    gamepad.startBackgroundUpdates()
+
+    leftSideMotors = threading.Thread(target=threadedMovement, args=(0,), daemon=True)
+    leftSideMotors.start()
+    rightSideMotors = threading.Thread(target=threadedMovement, args=(1,), daemon=True)
+    rightSideMotors.start()
+
+    try:
+        while True:
+            time.sleep(pollInterval)
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+         # Ensure the background thread is always terminated when we are done
+         gamepad.disconnect()
